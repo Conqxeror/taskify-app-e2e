f2014df1a3e0062cdfacbeb13bd50e6e1160bd27
Wali Mohammad Kadri <wali.mohammad@freighting.in>
2023-12-30 00:38:05 +0530

    added the sidebar

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { OrgControl } from './_components/org-control'
+
+export default function OrganizationIdLayout({children}: {children: React.ReactNode}) {
+  return (
+    <div>
+        <OrgControl/>
+        {children}
+    </div>
+  )
+}
